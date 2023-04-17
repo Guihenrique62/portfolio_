903150532e2624fc17c58cef0d6cce6f44e8bac9
Guilherme Henrique <103789281+Guihenrique62@users.noreply.github.com>
2023-04-16 21:50:17 -0300

    theme-colors

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
 import './styles/style.css';
 import slider from './scripts/slider';
+import Typed from 'typed.js';
+import handlerTheme from './scripts/handlerTheme.js';
 
+document.getElementById('theme-button').addEventListener('click', handlerTheme)
+
+
+
+//Writes the loop of strings
+const typed = new Typed('.multiple-text',{
+  strings: ['Frontend Developer', 'Freelancer', 'Web Developer'],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true
+
+});
 
 
